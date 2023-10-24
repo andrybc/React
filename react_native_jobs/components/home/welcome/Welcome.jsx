bc944ae763b23af87ce1eaeee600a1d9584109df
andrybc <canelbandry@gmail.com>
2023-10-23 23:31:37 -0400

    react native start. Doing a job search app. Learning the fundementals as of now

---
+++ react_native_jobs/components/home/welcome/Welcome.jsx
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+import { 
+  View, 
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList
+   } from 'react-native'
+
+import styles from './welcome.style'
+import { useRouter } from 'expo-router'
+import {icons, SIZES} from '../../../constants'
+
+const Welcome = () => {
+  const router = useRouter();
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}> Hello Andry </Text>
+        <Text style={styles.welcomeMessage}> Find your perfect job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder='What are you looking for?'
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={()=>{}}>
+          <Image
+          source={icons.search}
+          resizeMode='contain'
+          style={styles.searchBtnImage}
+          >
+
+          </Image>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+export default Welcome
